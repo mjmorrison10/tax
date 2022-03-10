2c46b93ee94bc74a0ff5cac51310a0b2a09c7ea5
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2022-03-09 16:44:23 -0800

    Update splitButton.js

--- components/splitButton.js
+++ components/splitButton.js
@@ -25,7 +25,6 @@ export default function SplitButton(props) {
   };
 
 
-//   test
   const handleClose = (event) => {
     if (anchorRef.current && anchorRef.current.contains(event.target)) {
       return;
