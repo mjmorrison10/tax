a7606cb8163ace16f6161b97f96a49db309ac40a
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2022-03-14 15:17:08 -0700

    Update noticeComp.js

--- components/commonIrsNotices/noticeComp.js
+++ components/commonIrsNotices/noticeComp.js
@@ -1,9 +1,15 @@
-import { ExpandMore } from '@mui/icons-material'
-import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import { ExpandMore } from "@mui/icons-material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 
-function NoticeComp({n, i}) {
-  console.log(n)
+function NoticeComp({ n, i }) {
+  console.log(n);
   const [expanded, setExpanded] = useState(false);
 
   const handleChange = (panel) => (event, isExpanded) => {
@@ -12,65 +18,68 @@ function NoticeComp({n, i}) {
 
   return (
     <Accordion
-    expanded={expanded === `panel${i}`}
-    onChange={handleChange(`panel${i}`)}
-    sx={{  width: '65ch' }}
-  >
-    <AccordionSummary
-      expandIcon={<ExpandMore />}
-      aria-controls="panel1bh-content"
-      id="panel1bh-header"
+      expanded={expanded === `panel${i}`}
+      onChange={handleChange(`panel${i}`)}
+      sx={{ width: "65ch" }}
     >
-      <Typography color='info.main' component={'h2'} fontWeight={'bold'} sx={{ width: "33%", flexShrink: 0 }}>
-        {n.label}
-      </Typography>
-      <Typography sx={{ color: "text.secondary" }}>
-        {n.title}
-      </Typography>
-    </AccordionSummary>
-    <AccordionDetails
-      sx={{ display: "flex", flexDirection: "column", gap: 1 }}
-    >
-      <Typography variant="body" component={'h3'} fontWeight={"bold"}>
-       {`What it's saying`}
-      </Typography>
-      <Typography variant="body">{n.saying}</Typography>
-      {n.saying2 && <Typography variant="body">{n.saying2}</Typography>}
+      <AccordionSummary
+        expandIcon={<ExpandMore />}
+        aria-controls="panel1bh-content"
+        id="panel1bh-header"
+      >
+        <Typography
+          color="info.main"
+          component={"h2"}
+          fontWeight={"bold"}
+          sx={{ width: "33%", flexShrink: 0 }}
+        >
+          {n.label}
+        </Typography>
+        <Typography
+          sx={{ color: "text.secondary", textAlign: { xs: "right", sm: null } }}
+        >
+          {n.title}
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails
+        sx={{ display: "flex", flexDirection: "column", gap: 1 }}
+      >
+        <Typography variant="body" component={"h3"} fontWeight={"bold"}>
+          {`What it's saying`}
+        </Typography>
+        <Typography variant="body">{n.saying}</Typography>
+        {n.saying2 && <Typography variant="body">{n.saying2}</Typography>}
 
-      {n.list && (
-        <Box component={"ul"} display={"flex"} flexDirection={"column"}>
-          {n.list && (
-            <Typography variant="body" component={"li"}>
-              {n.list}
-            </Typography>
-          )}
-          {n.list2 && (
-            <Typography variant="body" component={"li"}>
-              {n.list2}
-            </Typography>
-          )}
-          {n.list3 && (
-            <Typography variant="body" component={"li"}>
-              {n.list3}
-            </Typography>
-          )}
-        </Box>
-      )}
+        {n.list && (
+          <Box component={"ul"} display={"flex"} flexDirection={"column"}>
+            {n.list && (
+              <Typography variant="body" component={"li"}>
+                {n.list}
+              </Typography>
+            )}
+            {n.list2 && (
+              <Typography variant="body" component={"li"}>
+                {n.list2}
+              </Typography>
+            )}
+            {n.list3 && (
+              <Typography variant="body" component={"li"}>
+                {n.list3}
+              </Typography>
+            )}
+          </Box>
+        )}
 
-      {n.saying3 && <Typography variant="body">{n.saying3}</Typography>}
-      <Typography variant="body" component={'h3'} fontWeight={"bold"}>
-        How to resolve it
-      </Typography>
-      <Typography variant="body">{n.resolve}</Typography>
-      {n.resolve2 && (
-        <Typography variant="body">{n.resolve2}</Typography>
-      )}
-      {n.resolve3 && (
-        <Typography variant="body">{n.resolve3}</Typography>
-      )}
-    </AccordionDetails>
-  </Accordion>
-  )
+        {n.saying3 && <Typography variant="body">{n.saying3}</Typography>}
+        <Typography variant="body" component={"h3"} fontWeight={"bold"}>
+          How to resolve it
+        </Typography>
+        <Typography variant="body">{n.resolve}</Typography>
+        {n.resolve2 && <Typography variant="body">{n.resolve2}</Typography>}
+        {n.resolve3 && <Typography variant="body">{n.resolve3}</Typography>}
+      </AccordionDetails>
+    </Accordion>
+  );
 }
 
-export default NoticeComp
+export default NoticeComp;
